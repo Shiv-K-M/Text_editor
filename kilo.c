292c34b2818ed7b9c7716d664c8e410b81b17229
Shiv-K-M <shivarajkumarmuniyal@gmail.com>
2024-06-28 16:25:54 +0530

    detect home and end keys

--- kilo.c
+++ kilo.c
@@ -18,9 +18,12 @@ enum editorKey {
      ARROW_LEFT = 1000,  
      ARROW_RIGHT,  
      ARROW_UP , 
-     ARROW_DOWN,
+     ARROW_DOWN, 
+     HOME_KEY,
+     END_KEY,  
      PAGE_UP, 
-     PAGE_DOWN  
+     PAGE_DOWN 
+      
 };   
 
 /*** data ***/
@@ -73,21 +76,39 @@ int  editorReadKey() {
   if (c == '\x1b') {
     char seq[3];
     if (read(STDIN_FILENO, &seq[0], 1) != 1) return '\x1b';
-    if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
-    if(seq[2] == '~'){  
-        switch(seq[1]){  
+    if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';  
+
+
+    if(seq[0] == '[') { 
+      if(seq[1] <= '0' && seq[1] <= '9') {  
+         if(read(STDIN_FILENO, &seq[2], 1)!=1) return '\x1b';
+         if(seq[2] == '~'){  
+        switch(seq[1]){ 
+            case '1': return HOME_KEY; 
+            case '4': return END_KEY;    
             case '5': return PAGE_UP;
-            case '6': return PAGE_DOWN;
-        }  
+            case '6': return PAGE_DOWN;  
+            case '7': return HOME_KEY;  
+            case '8': return END_KEY; 
+           } 
+         } 
     }  
      else {
       switch ( seq[1]) {
         case 'A': return ARROW_UP;
         case 'B': return ARROW_DOWN;
         case 'C': return ARROW_RIGHT;
-        case 'D': return ARROW_LEFT;
+        case 'D': return ARROW_LEFT;  
+        case 'H': return HOME_KEY; 
+        case 'F': return END_KEY;  
       }
-    }
+     } 
+    } else if(seq[0] == '0') { 
+           switch(seq[1]) { 
+              case 'H': return HOME_KEY; 
+              case 'F': return END_KEY; 
+              }
+          } 
     return '\x1b';
   } else {
     return c;
