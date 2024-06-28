64625962ed7faa6b2132cfa0c31b2802da5cef4e
Shiv-K-M <shivarajkumarmuniyal@gmail.com>
2024-06-28 16:31:55 +0530

    map Home & End keys to cursor

--- kilo.c
+++ kilo.c
@@ -248,7 +248,14 @@ void editorMoveCursor( int key){
       write(STDOUT_FILENO, "\x1b[2J", 4);
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
-      break;
+      break;  
+
+    case HOME_KEY: 
+        E.cx=0;
+        break;
+    case END_KEY: 
+        E.cx= E.screencols -1;
+       break; 
 
     case PAGE_UP:
     case PAGE_DOWN:
