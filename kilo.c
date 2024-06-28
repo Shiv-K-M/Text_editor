116b351cf69b0dd24c64f6ca1933b5cb2e48c1f4
Shiv-K-M <shivarajkumarmuniyal@gmail.com>
2024-06-28 17:05:06 +0530

    {}

--- kilo.c
+++ kilo.c
@@ -153,7 +153,22 @@ int getWindowSize(int *rows, int *cols) {
     *rows = ws.ws_row;
     return 0;
   }
+} 
+
+/*** file i/o ***/
+
+void editorOpen(){ 
+      char *line = "Hello world!" ;
+      ssize_t linelen = 13;
+
+      E.row.size = linelen;
+      E.row.chars= malloc(linelen +1); 
+      memcpy(E.row.chars, line , linelen);  
+      E.row.chars[linelen] = '\0'; 
+      E.numrows = 1;
 }  
+
+
 /*** append buffer ***/
 
 struct abuf{
@@ -295,11 +310,12 @@ void initEditor() {
 
 int main() {
   enableRawMode();
-  initEditor();
+  initEditor();  
+  editorOpen(); 
 
   while (1) {
     editorRefreshScreen();
-    editorProcessKeypress();
+    editorProcessKeypress();  
   }
 
   return 0;
